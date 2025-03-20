f33f7af2e3a9137406e85b51c55ea1e2cfca6e6b
pritam dutta <webdevpritam7498@gmail.com>
2025-03-20 19:48:14 +0600

    data come for db

--- src/components/utils/Description.js
+++ src/components/utils/Description.js
@@ -1,6 +1,6 @@
 const ServiceDescription = ({ title, subtitle, description }) => {
   return (
-    <div className="text-center my-5 w-[717px] mx-auto">
+    <div className="text-center w-[717px] mx-auto my-10">
       <h4 className="text-red-500 text-xl font-semibold">---- {subtitle} ----</h4>
       <h2 className="font-bold text-[45px]">{title}</h2>
       <p className="text-gray-500 text-[16px] tracking-wide">{description}</p>
